6d62e8cfe1b10b98533800f1514a814f07cc6c43
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2024-06-27 17:39:07 +0200

    Fix support check

--- src/index.js
+++ src/index.js
@@ -1,96 +1,76 @@
 function polyfill({
-  fills,
-  url,
-  options = '',
-  minify = true,
-  rum = true,
-  agent,
-  agentFallback,
-  onSuccess,
-  onError,
-}) {
-  if (!fills) {
-    throw new Error('No fills specified.');
-  }
+                      fills,
+                      url,
+                      options = '',
+                      minify = true,
+                      rum = true,
+                      agent,
+                      agentFallback,
+                      onSuccess,
+                      onError,
+                  }) {
+    if (!fills) {
+        throw new Error('No fills specified.');
+    }
 
-  const fillAnyway = options.indexOf('always') >= 0 || agent; // check if 'always' flag or agent is set
-  const neededPolyfills = fillAnyway ? fills : checkSupport(fills);
+    const fillAnyway = options.indexOf('always') >= 0 || agent; // check if 'always' flag or agent is set
+    const neededPolyfills = fillAnyway ? fills : checkSupport(fills);
 
 
-  if (neededPolyfills.length > 0) {
-    return loadScript({
-      neededPolyfills, minify, fills, options, rum, agent, agentFallback, onSuccess, onError, url
-    });
-  }
+    if (neededPolyfills.length > 0) {
+        return loadScript({
+            neededPolyfills, minify, fills, options, rum, agent, agentFallback, onSuccess, onError, url
+        });
+    }
 
-  return onSuccess();
+    return onSuccess();
 }
 
 
 function checkSupport(fills) {
-  let unsupportedFills = [];
-
-  for (let i = 0; i < fills.length; i++) {
-    const fill = fills[i];
-    const parts = fill.split('.'); // i.e. ['Array', 'prototype', 'includes']
-    const type = parts[0];
-    const prototype = parts[1] === 'prototype';
-    const method = parts[2];
-
-    let isSupported = false;
-
-    // check for special test cases, otherwise use regular reduce function against window
-    switch (type) {
-      case 'Intl':
-        isSupported = window[type];
-        break;
-
-      case 'Element':
-        isSupported = 'Element' in window;
-        if (prototype && isSupported) {
-          isSupported = method in Element.prototype;
+    return fills.filter(fill => {
+        const parts = fill.split('.'); // i.e. ['Array', 'prototype', 'includes']
+        let obj = window;
+        for (const index in parts) {
+            if (!(parts[index] in obj)) {
+                return true;
+            }
+            if (index < parts.length - 1) {
+                obj = obj[parts[index]];
+            }
         }
-        break;
-
-      default:
-        isSupported = parts.reduce((key, value) => key[value], window);
-    }
-
-    if (!isSupported) {
-      unsupportedFills.push(fill);
-    }
-  }
-
-  return unsupportedFills;
+        return false;
+    });
 }
 
 
 function loadScript(args) {
-  const min      = args.minify  ? '.min' : '';
-  const features = args.fills   ? `features=${args.neededPolyfills.join(',')}` : '';
-  const flags    = args.options ? `\&flags=${args.options.join(',')}` : '';
-  const monitor  = args.rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads RUM scripts anyway
-  const agent    = args.agent   ? `\&ua=${args.agent}` : '';
-  const fallback = args.agentFallback ? `\&unknown=${args.agentFallback}` : '';
-  const url = args.url ? args.url : 'https://cdnjs.cloudflare.com/polyfill/v3';
+    const min = args.minify ? '.min' : '';
+    const features = args.fills ? `features=${args.neededPolyfills.join(',')}` : '';
+    const flags = args.options ? `\&flags=${args.options.join(',')}` : '';
+    const monitor = args.rum ? '\&rum=1' : ''; // not set to rum=0 since it loads RUM scripts anyway
+    const agent = args.agent ? `\&ua=${args.agent}` : '';
+    const fallback = args.agentFallback ? `\&unknown=${args.agentFallback}` : '';
+    const url = args.url ? args.url : 'https://cdnjs.cloudflare.com/polyfill/v3';
 
-  const js = document.createElement('script');
+    const js = document.createElement('script');
 
-  js.src = `${url}/polyfill${min}.js?${features + flags + monitor + agent + fallback}`;
-  js.async = true;
+    js.src = `${url}/polyfill${min}.js?${features + flags + monitor + agent + fallback}`;
+    js.async = true;
 
-  document.body.appendChild(js);
+    document.body.appendChild(js);
 
-  js.onload = () => args.onSuccess();
-  js.onerror = () => {
-      const error = 'Error loading polyfills. Open a ticket: https://github.com/PascalAOMS/dynamic-polyfill/issues';
-      if (args.onError) {
-          args.onError(error);
-      } else {
-          throw new Error(error);
-      }
-  };
+    js.onload = () => args.onSuccess();
+    js.onerror = () => {
+        const error = 'Error loading polyfills. Open a ticket: https://github.com/PascalAOMS/dynamic-polyfill/issues';
+        if (args.onError) {
+            args.onError(error);
+        } else {
+            throw new Error(error);
+        }
+    };
 }
 
 
 module.exports = polyfill;
+
